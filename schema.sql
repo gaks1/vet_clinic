7e9ed79ed15ad281381412b041dfc694f29afa3e
isaacg <iwanyoike65@gmail.com>
2023-05-01 18:29:02 +0300

    Insert data

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-  id INTEGER PRIMARY KEY,
+  id INTEGER GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(255),
   date_of_birth DATE,
   escape_attempts INTEGER,
   neutered BOOLEAN,
-  weight_kg DECIMAL(10, 2)
+  weight_kg DECIMAL(10, 2),
+  PRIMARY KEY (id)
 );
